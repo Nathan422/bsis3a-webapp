bf88c27f1e72532ffe3a666cd9bd98314268b05b
Nathan422 <jonathanpecolera@gmail.com>
2022-06-07 18:18:54 +0800

    add crud

--- Models/Type.cs
+++ Models/Type.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace bsis3a_webapp.Models
@@ -8,6 +9,7 @@ namespace bsis3a_webapp.Models
         public int Id { get; set; }
         [Required]
         [StringLength(50)]
+        [DisplayName("Type Name")]
         public string Name { get; set; }
         public Item Item { get; set; }
         public int ItemId { get; set; }
---
+++ Models/ViewModel/TypeViewModel.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using bsis3a_webapp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace bsis3a_webapp.Models.ViewModels
+
+{
+    public class TypeViewModel
+    {
+        public Type Type { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+
+        public IEnumerable<SelectListItem> selectListItem(IEnumerable<Item> Item)
+        {
+            List<SelectListItem> ItemList = new List<SelectListItem>();
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "Select Item",
+                Value = "0"
+            };
+            ItemList.Add(sli);
+            foreach(Item item in Items)
+            {
+                sli = new SelectListItem
+            {
+                Text = item.Name,
+                Value = item.Id.ToString()
+            };
+             ItemList.Add(sli);
+            }
+            return ItemList;
+        }
+    }
+}
